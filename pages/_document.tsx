40777ac9fe188ac2be866aa9b398db4dffb9c43a
williamb301 <wboudy1@jhu.edu>
2025-07-13 17:46:22 -0700

    AdSense Verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9121376313550113"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
